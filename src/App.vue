582337feabd0886647502a2f8b9653a3ca0f3e52
krystalry <2745147939@qq.com>
2019-02-14 20:25:22 +0800

    ignore package.json

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Header from "./components/Header.vue"
+  export default {
+    name: 'App',
+    component: {
+      Header
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于我们</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: "Header"
+  }
+</script>
+
+<style>
+  .header {
+    height: 50px;
+    background-color: #5a5555;
+  }
+
+  img {
+    max-width: 120px;
+    margin-left: 50px;
+    margin-top: 10px;
+  }
+
+  ul {
+    float: right;
+    list-style: none;
+  }
+
+  li{
+    display: inline-block;
+    padding: 10px 15px;
+  }
+
+  a{
+    color: #cccccc;
+    font-size: 14px;
+    text-shadow: nonr;
+    text-decoration: none;
+  }
+</style>
